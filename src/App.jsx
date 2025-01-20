934ecc273aceb50d6bdb7503878b4948c375e2f9
Adnan Iliyasu Muhammad <adnanilyas969@gmail.com>
2025-01-20 20:28:22 +0100

    fix deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       Report for
+  Jeremy Robson
+
+  Daily
+  Weekly
+  Monthly
+
+  Work
+  5hrs  {/* daily  */}
+  Previous - 7hrs  {/* daily  */}
+  32hrs {/* weekly  */}
+  Previous - 36hrs {/* weekly  */}
+  103hrs {/* monthly  */}
+  Previous - 128hrs {/* monthly  */}
+
+  Play
+  1hr  {/* daily  */}
+  Previous - 2hrs  {/* daily  */}
+  10hrs {/* weekly  */}
+  Previous - 8hrs {/* weekly  */}
+  23hrs {/* monthly  */}
+  Previous - 29hrs {/* monthly  */}
+
+  Study
+  0hrs  {/* daily  */}
+  Previous - 1hr  {/* daily  */}
+  4hrs {/* weekly  */}
+  Previous - 7hrs {/* weekly  */}
+  13hrs {/* monthly  */}
+  Previous - 19hrs {/* monthly  */}
+
+  Exercise
+  1hr  {/* daily  */}
+  Previous - 1hr  {/* daily  */}
+  4hrs {/* weekly  */}
+  Previous - 5hrs {/* weekly  */}
+  11hrs {/* monthly  */}
+  Previous - 18hrs {/* monthly  */}
+
+  Social
+  1hr  {/* daily  */}
+  Previous - 3hrs  {/* daily  */}
+  5hrs {/* weekly  */}
+  Previous - 10hrs {/* weekly  */}
+  21hrs {/* monthly  */}
+  Previous - 23hrs {/* monthly  */}
+
+  Self Care
+  0hrs  {/* daily  */}
+  Previous - 1hr  {/* daily  */}
+  2hrs {/* weekly  */}
+  Previous - 2hrs {/* weekly  */}
+  7hrs {/* monthly  */}
+  Previous - 11hrs {/* monthly  */}
     </>
   )
 }
