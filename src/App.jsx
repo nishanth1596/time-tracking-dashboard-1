70d25794e6d565401881827b588bc7bf055bfa0f
nishanth1596 <121683297+nishanth1596@users.noreply.github.com>
2025-01-30 15:34:16 +0530

    Added header, main and footer to improve accessibility.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ function App() {
  
   return (
     <>
-     <Container />
+ <header className="sr-only">
+        <h1>Time Tracking App</h1>
+      </header>
+      <main>
+        <Container />
+      </main>
+      <footer className="absolute animate-pulse bottom-0 left-1/2 w-full -translate-x-1/2  text-center text-white">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://www.frontendmentor.io/profile/Adnan0-IM">
+          Adnan Iliyasu Muhammad
+        </a>
+        .
+      </footer>
     </>
   );
 }
